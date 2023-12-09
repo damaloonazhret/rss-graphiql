e37f3f58d23cbae6fedf3576d7c6d34c52d34134
Denys Bilonozhko <deformix@gmail.com>
2023-12-09 18:00:52 +0200

    feat: user registration functionality

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,22 +1,34 @@
 import { initializeApp } from 'firebase/app';
-const {
-  VITE_FIREBASE_API_KEY,
-  VITE_AUTH_DOMAIN,
-  VITE_PROJECT_ID,
-  VITE_STORAGE_BUCKET,
-  VITE_MESSAGING_SENDER_ID,
-  VITE_APP_ID,
-} = import.meta.env;
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { collection, addDoc, getFirestore } from 'firebase/firestore';
+const ENV = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: VITE_FIREBASE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_MESSAGING_SENDER_ID,
-  appId: VITE_APP_ID,
+  apiKey: ENV.VITE_FIREBASE_API_KEY,
+  authDomain: ENV.VITE_AUTH_DOMAIN,
+  projectId: ENV.VITE_PROJECT_ID,
+  storageBucket: ENV.VITE_STORAGE_BUCKET,
+  messagingSenderId: ENV.VITE_MESSAGING_SENDER_ID,
+  appId: ENV.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-export default app;
+const registerUser = async (email: string, password: string): Promise<boolean | string> => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      authProvider: 'local',
+      email,
+    });
+    return true;
+  } catch (e) {
+    return 'Registration failed.';
+  }
+};
+
+export { registerUser };
