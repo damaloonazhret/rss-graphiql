2900b9ce29e2f6a890af2308d1372aad77d113fa
Denys Bilonozhko <deformix@gmail.com>
2023-12-10 18:26:10 +0200

    feat: add redirection

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,16 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import LoginForm from '../components/loginForm/LoginForm';
+import { auth } from '../services/firebase';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
+  if (user) {
+    navigate('/');
+  }
+
   return <LoginForm />;
 };
 
--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,6 +1,16 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import RegistrationForm from '../components/registrationForm/RegistrationForm';
+import { auth } from '../services/firebase';
 
 const RegistrationPage = () => {
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
+  if (user) {
+    navigate('/');
+  }
+
   return <RegistrationForm />;
 };
 
