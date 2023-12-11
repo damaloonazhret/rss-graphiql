3fd808aca4a85d50084b0f3a097b3ef936f3027d
Denys Bilonozhko <deformix@gmail.com>
2023-12-11 17:26:40 +0200

    refactor: Header component conditionally render GraphiQL NavLink based on user

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,9 +12,11 @@ const Header = () => {
         <NavLink className={styles['nav-item']} to="/">
           Welcome
         </NavLink>
-        <NavLink className={styles['nav-item']} to="/graphiql">
-          GraphiQL
-        </NavLink>
+        {user && (
+          <NavLink className={styles['nav-item']} to="/graphiql">
+            GraphiQL
+          </NavLink>
+        )}
       </div>
       <div>
         {user ? (
