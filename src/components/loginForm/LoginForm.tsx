4286738105cddd71c7c60a21f5f4676972b21009
Denys Bilonozhko <deformix@gmail.com>
2023-12-22 19:21:57 +0200

    feat: change form styles

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -4,28 +4,23 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { loginUser } from '../../services/firebase';
 import { loginSchema } from '../../services/validation';
 import styles from './login-form.module.css';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { LanguageContext } from '../../context/localization';
 import { Box, Button } from '@mui/material';
 import { showToastError } from '../../services/toasts';
 import { FirebaseError } from 'firebase/app';
 
 const LoginForm = () => {
-  const { language, languageData } = useContext(LanguageContext);
+  const { languageData } = useContext(LanguageContext);
   const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
   } = useForm({
     resolver: yupResolver(loginSchema(languageData)),
     mode: 'onChange',
   });
-
-  useEffect(() => {
-    reset();
-  }, [language]);
 
   const handleFormSubmit = async (data: { email: string; password: string }) => {
     try {
@@ -43,28 +38,38 @@ const LoginForm = () => {
   };
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <form onSubmit={handleSubmit(handleFormSubmit)} noValidate>
-        <h1>{languageData.login}</h1>
-        <Box component="label" sx={{ display: 'block' }} htmlFor="email">
-          {languageData.email}:
-        </Box>
-        <input type="text" {...register('email')} id="email" />
-        <div className={styles['field-error']}>
-          <p>{errors.email?.message}</p>
-        </div>
-        <Box component="label" sx={{ display: 'block' }} htmlFor="password">
-          {languageData.password}:
-        </Box>
-        <input type="password" {...register('password')} id="password" />
-        <div className={styles['field-error']}>
-          <p>{errors.password?.message}</p>
-        </div>
+    <Box
+      component="form"
+      onSubmit={handleSubmit(handleFormSubmit)}
+      noValidate
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2px',
+        alignItems: 'center',
+        maxWidth: '200px',
+        margin: '0 auto',
+      }}
+    >
+      <h1>{languageData.login}</h1>
+      <Box component="label" sx={{ display: 'block' }} htmlFor="email">
+        {languageData.email}:
+      </Box>
+      <input type="text" {...register('email')} id="email" />
+      <div className={styles['field-error']}>
+        <p>{errors.email?.message}</p>
+      </div>
+      <Box component="label" sx={{ display: 'block' }} htmlFor="password">
+        {languageData.password}:
+      </Box>
+      <input type="password" {...register('password')} id="password" />
+      <div className={styles['field-error']}>
+        <p>{errors.password?.message}</p>
+      </div>
 
-        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
-          {languageData.login}
-        </Button>
-      </form>
+      <Button type="submit" variant="contained" sx={{ mt: '6px' }}>
+        {languageData.login}
+      </Button>
     </Box>
   );
 };
--- src/components/registrationForm/RegistrationForm.tsx
+++ src/components/registrationForm/RegistrationForm.tsx
@@ -4,29 +4,24 @@ import { registerUser } from '../../services/firebase';
 import { registerSchema } from '../../services/validation';
 import styles from './registration-form.module.css';
 import { useNavigate } from 'react-router-dom';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { LanguageContext } from '../../context/localization';
 import { Box, Button } from '@mui/material';
 import { showToastError } from '../../services/toasts';
 import { FirebaseError } from 'firebase/app';
 
 const RegistrationForm = () => {
-  const { language, languageData } = useContext(LanguageContext);
+  const { languageData } = useContext(LanguageContext);
   const navigate = useNavigate();
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
   } = useForm({
     resolver: yupResolver(registerSchema(languageData)),
     mode: 'onChange',
   });
-
-  useEffect(() => {
-    reset();
-  }, [language]);
 
   const handleFormSubmit = async (data: { name: string; email: string; password: string }) => {
     try {
@@ -44,42 +39,52 @@ const RegistrationForm = () => {
   };
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <form onSubmit={handleSubmit(handleFormSubmit)} noValidate>
-        <h1>{languageData.register}</h1>
-        <Box component="label" sx={{ display: 'block' }} htmlFor="name">
-          {languageData.name}:
-        </Box>
-        <input type="text" {...register('name')} id="name" />
-        <div className={styles['field-error']}>
-          <p>{errors.name?.message}</p>
-        </div>
-        <Box component="label" sx={{ display: 'block' }} htmlFor="email">
-          {languageData.email}:
-        </Box>
-        <input type="text" {...register('email')} id="email" />
-        <div className={styles['field-error']}>
-          <p>{errors.email?.message}</p>
-        </div>
-        <Box component="label" sx={{ display: 'block' }} htmlFor="password">
-          {languageData.password}:
-        </Box>
-        <input type="password" {...register('password')} id="password" />
-        <div className={styles['field-error']}>
-          <p>{errors.password?.message}</p>
-        </div>
-        <Box component="label" sx={{ display: 'block' }} htmlFor="passwordConfirm">
-          {languageData.passwordConfirm}:
-        </Box>
-        <input type="password" {...register('passwordConfirm')} id="passwordConfirm" />
-        <div className={styles['field-error']}>
-          <p>{errors.passwordConfirm?.message}</p>
-        </div>
+    <Box
+      component="form"
+      onSubmit={handleSubmit(handleFormSubmit)}
+      noValidate
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2px',
+        alignItems: 'center',
+        maxWidth: '200px',
+        margin: '0 auto',
+      }}
+    >
+      <h1>{languageData.register}</h1>
+      <Box component="label" sx={{ display: 'block' }} htmlFor="name">
+        {languageData.name}:
+      </Box>
+      <input type="text" {...register('name')} id="name" />
+      <div className={styles['field-error']}>
+        <p>{errors.name?.message}</p>
+      </div>
+      <Box component="label" sx={{ display: 'block' }} htmlFor="email">
+        {languageData.email}:
+      </Box>
+      <input type="text" {...register('email')} id="email" />
+      <div className={styles['field-error']}>
+        <p>{errors.email?.message}</p>
+      </div>
+      <Box component="label" sx={{ display: 'block' }} htmlFor="password">
+        {languageData.password}:
+      </Box>
+      <input type="password" {...register('password')} id="password" />
+      <div className={styles['field-error']}>
+        <p>{errors.password?.message}</p>
+      </div>
+      <Box component="label" sx={{ display: 'block' }} htmlFor="passwordConfirm">
+        {languageData.passwordConfirm}:
+      </Box>
+      <input type="password" {...register('passwordConfirm')} id="passwordConfirm" />
+      <div className={styles['field-error']}>
+        <p>{errors.passwordConfirm?.message}</p>
+      </div>
 
-        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
-          {languageData.register}
-        </Button>
-      </form>
+      <Button type="submit" variant="contained" sx={{ mt: '6px' }}>
+        {languageData.register}
+      </Button>
     </Box>
   );
 };
