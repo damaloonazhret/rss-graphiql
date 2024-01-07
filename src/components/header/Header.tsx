099fa8f6a61bc5a815bc43753342b9470703d003
Denys Bilonozhko <deformix@gmail.com>
2024-01-07 18:48:25 +0200

    refactor: localization update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
       <div className={styles.mainMenu}>
         <NavLink to="/">
           <Button className={styles.button} variant="contained">
-            <Typography textAlign="center">{languageData.welcome}</Typography>
+            <Typography textAlign="center">{languageData.welcomePage}</Typography>
           </Button>
         </NavLink>
         {user && (
